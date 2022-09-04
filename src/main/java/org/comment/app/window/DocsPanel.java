fdcff5d6eb9da248f46bb79362a323ed7f191646
MidasMidas <sokfido@gmail.com>
2022-09-04 20:42:14 +0800

    optimize get line number function

--- src/main/java/org/comment/app/window/DocsPanel.java
+++ src/main/java/org/comment/app/window/DocsPanel.java
@@ -2,18 +2,28 @@ package org.comment.app.window;
 
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Element;
 import javax.swing.text.Utilities;
 
 public class DocsPanel extends JTextPane {
 
 
-    public int getCursorLineNumber() throws BadLocationException {
-        int caretPos = this.getCaretPosition();
-        int rowNum = (caretPos == 0) ? 1 : 0;
-        for (int offset = caretPos; offset > 0; ) {
-            offset = Utilities.getRowStart(this, offset) - 1;
-            rowNum++;
-        }
-        return rowNum;
+    /// old method, not good for use
+    //    public int getCursorLineNumber() throws BadLocationException {
+//        int caretPos = this.getCaretPosition();
+//        int rowNum = (caretPos == 0) ? 1 : 0;
+//        for (int offset = caretPos; offset > 0; ) {
+//            offset = Utilities.getRowStart(this, offset) - 1;
+//            rowNum++;
+//        }
+//        return rowNum;
+//    }
+    public int getCursorLineNumber() {
+        int caretPosition = this.getCaretPosition();
+        Element root = this.getDocument().getDefaultRootElement();
+        int index = root.getElementIndex(caretPosition);
+//        Element line = root.getElement( index );
+
+        return index + 1;
     }
 }
